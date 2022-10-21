cf4f0efe1cabc14c66bdf186164c085fbf7e7065
Dominika Rycerz <dominikarycerz@hotmail.com>
2022-10-22 00:07:31 +0200

    Add interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   template: ` <h1>Welcome to {{ title }}!</h1>
     <app-input-button-unit></app-input-button-unit>
     <ul>
-      <li *ngFor="let todoItem of todoList">{{ todoItem.title }}</li>
+      <li *ngFor="let todoItem of todoList">
+        <app-todo-item [item]="todoItem"></app-todo-item>
+      </li>
     </ul>`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
-  todoList = [
+  todoList: TodoItem[] = [
     { title: 'install NodeJS' },
     { title: 'install Angular CLI' },
     { title: 'create new app' },
