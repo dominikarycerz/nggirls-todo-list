60d76fbe8b7625f0e22c9649b283fc828551e38c
Dominika Rycerz <dominikarycerz@hotmail.com>
2022-10-21 22:45:32 +0200

    Add title to the input-button-unit

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: `The title is {{ title }}`,
+  template: `<p>The title is {{ title }}</p>
+    <input [value]="title" />
+    <button>Save</button>`,
   styleUrls: ['./input-button-unit.component.scss'],
 })
 export class InputButtonUnitComponent implements OnInit {
   title = 'Hello world';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.title = 'This is not a title you are looking for';
+    }, 3000);
+  }
 }
